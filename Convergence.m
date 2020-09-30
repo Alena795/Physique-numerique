9e8c9c04e1dd5bcf81c4f9d587ad1df0d806a2af
PierreAncey <37844847+PierreAncey@users.noreply.github.com>
2020-09-30 16:23:29 +0200

    Update Convergence.m

--- Convergence.m
+++ Convergence.m
@@ -2,9 +2,9 @@
 filename = 'ValeursMatLab.txt';
 data = load(filename);
 
-%Définit les variables associées aux valeurs dans MatLab
+%Défini les variables associées aux valeurs dans MatLab
 nsteps_num = data(:,1); 
-xfin_num = data(:,1);
+xfin_num = data(:,2);
 
 %Génération du graphe
 lw=2; fs=16;
@@ -12,7 +12,7 @@ figure
 plot(1./nsteps_num, xfin_num, 'k+-','linewidth',lw)
 set(gca,'fontsize',fs)
 xlabel('1/N_{steps}')
-ylabel('x_{final}')
+ylabel('x_{final} [m]')
 grid on
 
 % si on a la solution analytique:
